c2fec1e905efb660dc0c4efdcd32999a1f52cb61
Sedanur SAMUR <81965223+sedanursamur@users.noreply.github.com>
2021-06-06 18:21:33 +0300

    Add files via upload

---
+++ extension_method/extension_method/Extensions/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extension_method.Extensions
+{
+    public static class Extensions
+    {
+      
+        public static string encryption(this string str,  int key) 
+        {
+            
+             char[] alphabet ={'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'y', 'z' };
+
+            string result = "";
+            foreach (var i in str)
+            { 
+                if (str.Contains(i))
+                {
+                    int number = Array.IndexOf(alphabet, i);
+                    number = (number + key) % 29;
+                    result = result + alphabet[number];
+
+                }
+                else { result += i; }
+                }
+                return result;
+
+            }
+        public static string decoder(this string str, int key)
+        {
+            return encryption(str, 29 - key);
+
+        }
+    }
+ }
+         
+
+
+
+
+  
---
+++ extension_method/extension_method/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using extension_method.Extensions;
+
+namespace extension_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartScreen();
+
+        }
+        static void StartScreen()
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :) ");
+            Console.WriteLine(" *******************************************");
+            Console.WriteLine("(1) Sezar Şifreleme");
+            Console.WriteLine("(2) Sezar Şifre Çözücü");
+           
+
+
+            int process = Convert.ToInt16(Console.ReadLine());
+            Router(process);
+
+        }
+    
+
+        static void Router(int process) 
+        {
+            switch (process)
+            {
+                case 1:
+                    get_text_encode();
+
+                     break;
+                case 2:
+                    get_text_decode();
+                    break; 
+                
+            }
+        }
+        public static void get_text_encode()
+        {
+            Console.WriteLine("Şifrelenecek metni giriniz:");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            Console.WriteLine("Şifreleme adımını giriniz :");
+            int key = Convert.ToInt16(Console.ReadLine());
+
+            //extension method olarak yazdığım şifrelemeye ulaşılır. Böylece klavyeden girilen yazı şifrelenebilir.
+            string result=str.encryption(key);
+
+            Console.WriteLine("Şifreli Hali:\n" + result);
+            Console.ReadLine();
+            StartScreen();
+
+
+        }
+        public static void get_text_decode()
+        {
+            Console.WriteLine("Şifresi çözülecek metni giriniz:");
+            string input = Console.ReadLine();
+            input= input.ToLower();
+            Console.WriteLine("Şifre çözücü adımını giriniz :");
+            int number = Convert.ToInt16(Console.ReadLine());
+
+            //extension method olarak yazdığım şifre çözücüye ulaşılır.Böylece klavyeden girilen yazının şifresi çözülebilir.
+            string result = input.decoder(number);
+
+
+            Console.WriteLine("Şifresi çözülmüş hali:\n" + result);
+            Console.ReadLine();
+            StartScreen();
+
+
+
+        }
+    }
+}
